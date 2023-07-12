8f0956bf5fa50a8bb6c35dca5812adabb582b618
Frierien <138498549+Frierien@users.noreply.github.com>
2023-07-12 15:55:49 +0800

    LogIn and Register ver 3

--- app/src/main/java/com/DhvsuDeals/ohayosekai/RegisterActivity.java
+++ app/src/main/java/com/DhvsuDeals/ohayosekai/RegisterActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -18,13 +19,20 @@ import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
-     private TextInputEditText editTextEmail, editTextPassword;
+     private TextInputEditText editTextEmail, editTextPassword, editTextConfirmPass;
      private Button btnReg;
      private ProgressBar ProgBar;
      private TextView View_LogIn;
+     private String User_ID;
+
 
     public void onStart() {
         super.onStart();
@@ -45,6 +53,7 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         editTextEmail = findViewById(R.id.txtEmail);
         editTextPassword = findViewById(R.id.txtPassword);
+        editTextConfirmPass = findViewById(R.id.txtConfirm_Password);
         btnReg = findViewById(R.id.btnRegister);
         ProgBar = findViewById(R.id.progressBar);
         View_LogIn = findViewById(R.id.LogInNow);
@@ -62,9 +71,10 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 ProgBar.setVisibility(View.VISIBLE);
-                String Click_email, Click_password;
+                String Click_email, Click_password, Click_ConfirmPass;
                 Click_email = String.valueOf(editTextEmail.getText());
                 Click_password = String.valueOf(editTextPassword.getText());
+                Click_ConfirmPass = String.valueOf(editTextConfirmPass.getText());
 
                 if (TextUtils.isEmpty(Click_email) || TextUtils.isEmpty(Click_password)){
                     Toast.makeText(RegisterActivity.this, "Missing some Credentials", Toast.LENGTH_SHORT).show();
@@ -72,36 +82,51 @@ public class RegisterActivity extends AppCompatActivity {
                     return;
                 }
 
-                /*mAuth.signInWithEmailAndPassword(Click_email, Click_password)
-                        .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                ProgBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(RegisterActivity.this, "Account Already Exist!!!", Toast.LENGTH_SHORT).show();
-                                    return;
-                                }
-                            }
-                        });*/
+                //check if the email inputed matches the EMAIL PATTERN Type
+                if (Patterns.EMAIL_ADDRESS.matcher(Click_email).matches()){
 
-                mAuth.createUserWithEmailAndPassword(Click_email, Click_password)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                ProgBar.setVisibility(View.GONE);
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(RegisterActivity.this, "Authentication Success!.",
-                                            Toast.LENGTH_SHORT).show();
-                                    Intent Go_LogIn = new Intent(getApplicationContext(), LogInActivity.class);
-                                    startActivity(Go_LogIn);
-                                    finish();
-                                } else {
+                    //check if passwords are matched
+                    if (Click_password.matches(Click_ConfirmPass)){
+                        mAuth.createUserWithEmailAndPassword(Click_email, Click_password)
+                                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        ProgBar.setVisibility(View.GONE);
+                                        if (task.isSuccessful()) {
+                                            User_ID = mAuth.getCurrentUser().getUid();//get the userUID on the firestore to be used as an user ID
+                                            DocumentReference SignUpRef_DB = FirebaseFirestore.getInstance().collection("Uses_ACCS_Information").document(User_ID);
+                                            Map<String, Object> SaveUser = new HashMap<>();
+                                            SaveUser.put("Mem-ID", User_ID);
+                                            SaveUser.put("Mem-Email", Click_email);
+                                            SaveUser.put("Mem-Password", Click_password);
 
-                                    Toast.makeText(RegisterActivity.this, "Authentication Failed!.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
+                                            SignUpRef_DB.set(SaveUser);
+
+                                            Toast.makeText(RegisterActivity.this, "Authentication Success!.", Toast.LENGTH_SHORT).show();
+                                            Intent Go_LogIn = new Intent(getApplicationContext(), LogInActivity.class);
+                                            startActivity(Go_LogIn);
+                                            finish();
+                                        } else {
+
+                                            Toast.makeText(RegisterActivity.this, "Authentication Failed!.",
+                                                    Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                });
+
+                    }else {
+                        editTextPassword.setError("Passwords doesn't Match!!");
+                        editTextPassword.requestFocus();
+                        ProgBar.setVisibility(View.GONE);
+                    }//end else of password matching
+
+                } else {
+                    editTextEmail.setError("Please Enter a Valid Email");
+                    editTextEmail.requestFocus();
+                    ProgBar.setVisibility(View.GONE);
+                }//end else of email pattern checking
+
+
             }
         });
     }
