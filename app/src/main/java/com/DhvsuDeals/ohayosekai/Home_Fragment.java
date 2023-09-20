9781d8be6b2dfd81c5f604a7f044d4d564c932b8
Frierien <138498549+Frierien@users.noreply.github.com>
2023-09-20 21:17:19 +0800

    Ver with Login UI (Viewpager in home Done)

--- app/src/main/java/com/DhvsuDeals/ohayosekai/Home_Fragment.java
+++ app/src/main/java/com/DhvsuDeals/ohayosekai/Home_Fragment.java
@@ -2,29 +2,16 @@ package com.DhvsuDeals.ohayosekai;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.viewpager2.widget.ViewPager2;
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.DhvsuDeals.ohayosekai.databinding.FragmentHomeBinding;
-import com.DhvsuDeals.ohayosekai.databinding.FragmentInboxBinding;
-import com.google.android.material.tabs.TabLayout;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
-import java.util.ArrayList;
+import com.DhvsuDeals.ohayosekai.databinding.FragmentHomeBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class Home_Fragment extends Fragment {
@@ -33,6 +20,7 @@ public class Home_Fragment extends Fragment {
     FirebaseFirestore db;
     ProgressDialog progressDialog;
     VP_Adapter_LOANS_SAVINGS vp_adapter_loans_savings;
+
 
 
     @Override
@@ -50,30 +38,8 @@ public class Home_Fragment extends Fragment {
         vp_adapter_loans_savings = new VP_Adapter_LOANS_SAVINGS(getActivity());
         HBinder.VPLoansSavings.setAdapter(vp_adapter_loans_savings);
 
-        HBinder.LoansSavingsTab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                HBinder.VPLoansSavings.setCurrentItem(tab.getPosition());
-            }
+        HBinder.VPLoansSavings.setPageTransformer(new PageTransformer());
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-
-        HBinder.VPLoansSavings.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                HBinder.LoansSavingsTab.getTabAt(position).select();
-            }
-        });
     }
 
 
